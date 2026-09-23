d399e81615917b5a61fd23d38cd95c8deba2f126
zero <pascage@yandex.com>
2026-09-23 14:07:23 +0700

    верстка Sidebar #20
    сверстал компонент Sidebar, отредактировал некоторые стили в index.css и добавил туда новые переменные для Sidebar
    добавил нужные иконки для sidebar по пути в папку icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,1 +1,157 @@
 // Sidebar.jsx - боковая панель: поиск, переключатели,
+import Logo from '../assets/icons/logo.svg'
+import InfoIcon from '../assets/icons/info.svg'
+import '../styles/Sidebar.css'
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__brand">
+            <img src={Logo} alt="AeroCast" className="sidebar__logo" />
+            <div className="sidebar__title">Aerocast</div>
+        </div>
+        <a href="#" className="sidebar__info-btn">
+            <img src={InfoIcon} className="sidebar__info-icon" />
+        </a>
+        
+      </div>
+
+      <section className="city-search">
+        <h2 className="city-search__title">Поиск города</h2>
+        <label htmlFor="city-search" className="city-search__label">
+          Поиск города
+        </label>
+        <input
+          id="city-search"
+          className="city-search__input"
+          type="search"
+          placeholder="Город"
+        />
+      </section>
+
+      <section className="data-source">
+        <h2 className="data-source__title">Источник данных</h2>
+        <label htmlFor="data-source" className="data-source__label">
+          Источник данных
+        </label>
+        <select id="data-source" className="data-source__select">
+          <option>WeatherAPI</option>
+        </select>
+      </section>
+
+
+      <section className="display-mode">
+        <h2 className="display-mode__title">Режим отображения</h2>
+        <div className="segmented">
+          <button type="button" className="segmented__btn">
+            Цветовой
+          </button>
+          <button type="button" className="segmented__btn">
+            Цифровой
+          </button>
+          <button type="button" className="segmented__btn segmented__btn--active">
+            Комбо
+          </button>
+        </div>
+      </section>
+
+
+      <section className="geo-nav">
+        <h2 className="geo-nav__title">Навигация</h2>
+        <button type="button" className="geo-nav__location-btn">Моё местоположение</button>
+      </section>
+
+      <section className="date-nav">
+        <h2 className="date-nav__title">Навигация по времени</h2>
+        <div className="days">
+          <button className="day-card" type="button">
+            <span className="day-card__label">ЧТ</span>
+            <span className="day-card__num">10</span>
+          </button>
+          <button className="day-card" type="button">
+            <span className="day-card__label">ПТ</span>
+            <span className="day-card__num">11</span>
+          </button>
+          <button className="day-card" type="button">
+            <span className="day-card__label">СБ</span>
+            <span className="day-card__num">12</span>
+          </button>
+          <button className="day-card day-card--active" type="button">
+            <span className="day-card__label">ВС</span>
+            <span className="day-card__num">13</span>
+            <span className="day-card__extra">сег</span>
+          </button>
+          <button className="day-card day-card--forecast" type="button">
+            <span className="day-card__label">ПН</span>
+            <span className="day-card__num">14</span>
+            <span className="day-card__extra">прог</span>
+          </button>
+          <button className="day-card day-card--forecast" type="button">
+            <span className="day-card__label">ВТ</span>
+            <span className="day-card__num">15</span>
+            <span className="day-card__extra">прог</span>
+          </button>
+          <button className="day-card day-card--forecast" type="button">
+            <span className="day-card__label">СР</span>
+            <span className="day-card__num">16</span>
+            <span className="day-card__extra">прог</span>
+          </button>
+        </div>
+      </section>
+
+      <section className="time-nav">
+        <h2 className="time-nav__title">Время суток</h2>
+        <div className="times">
+          <button className="time" type="button">00:00</button>
+          <button className="time" type="button">03:00</button>
+          <button className="time" type="button">06:00</button>
+          <button className="time" type="button">09:00</button>
+          <button className="time" type="button">12:00</button>
+          <button className="time" type="button">15:00</button>
+          <button className="time time--active" type="button">18:00</button>
+          <button className="time" type="button">21:00</button>
+        </div>
+      </section>
+
+      <section className="aqi-scale">
+        <h2 className="aqi-scale__title">Шкала AQI</h2>
+        <ul className="aqi-scale__list">
+          <li className="aqi-row">
+            <span className="aqi-row__dot" style={{ backgroundColor: '#22c55e' }} />
+            <span className="aqi-row__range">0–50</span>
+            <span className="aqi-row__label">Хорошее</span>
+          </li>
+          <li className="aqi-row">
+            <span className="aqi-row__dot" style={{ backgroundColor: '#eab308' }} />
+            <span className="aqi-row__range">51–100</span>
+            <span className="aqi-row__label">Умеренное</span>
+          </li>
+          <li className="aqi-row">
+            <span className="aqi-row__dot" style={{ backgroundColor: '#f97316' }} />
+            <span className="aqi-row__range">101–150</span>
+            <span className="aqi-row__label">Нездорово*</span>
+          </li>
+          <li className="aqi-row">
+            <span className="aqi-row__dot" style={{ backgroundColor: '#ef4444' }}/>
+            <span className="aqi-row__range">151–200</span>
+            <span className="aqi-row__label">Нездорово</span>
+          </li>
+          <li className="aqi-row">
+            <span className="aqi-row__dot" style={{ backgroundColor: '#a855f7' }}  />
+            <span className="aqi-row__range">201–300</span>
+            <span className="aqi-row__label">Очень плохое</span>
+          </li>
+          <li className="aqi-row">
+            <span className="aqi-row__dot" style={{ backgroundColor: '#7f1d1d' }}  />
+            <span className="aqi-row__range">300+</span>
+            <span className="aqi-row__label">Опасное</span>
+          </li>
+        </ul>
+      </section>
+    </aside>
+  );
+}
+
+export default Sidebar;
+
